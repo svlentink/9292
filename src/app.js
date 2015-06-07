c9b9b830f15b7d2094369666f9e5a767584ec0cc
SV Lentink <svlentink@gmail.com>
2015-06-07 18:14:59 +0200

    Added configuration option (broken commit)
    Current issue (run in browser console): $.ajax({url:'https://api.9292.nl/0.1/locations?lang=nl-NL&latlong=52.37848714700854%2C4.900076210975617&rows=1',dataType:'jsonp'}).always(function(data){console.log(data);})

--- src/app.js
+++ src/app.js
@@ -31,6 +31,7 @@ var UI = require('ui'),
 	Vector2 = require('vector2'),
 	statusBarHeight = 18, //pixels
 	ajax = require('ajax'),
+	configURL = 'http://svlentink.co.nf/pebble9292',
 	userPreferences = {
 		updateScreenIntervalInSec : 60,
 		extraInterChangeTime : true,
@@ -49,7 +50,10 @@ var UI = require('ui'),
 				location : "amsterdam/halvemaansteeg-19" // https://api.9292.nl/0.1/locations?lang=nl-NL&latlong=52.36660,4.89640
 			}
 		]
-	};
+	},
+	//vars to easy update the api, when changed
+	version = '0.1',
+	baseUrl = 'https://api.9292.nl/';
 
 
 
@@ -66,9 +70,6 @@ var statusBar = new UI.TimeText({
 });
 
 
-/**
-* Please update this method, make it all look pretty
-*/
 function showJourneyDataOnScreen(receivedData){
 	log.debug('Entering showJourneyDataOnScreen',receivedData);
 
@@ -98,9 +99,17 @@ function showJourneyDataOnScreen(receivedData){
 function getBestJourney(journeys){
 	log.debug('Entering getBestJourney', journeys);
 
+	function getNow(){ //because Date.now() is GMT, not Amsterdam time zone
+		var date = new Date(),
+			str =  date.getFullYear() + '-' + (date.getMonth() + 1).preZeros(2) + //month start with 0 in js
+			'-' + date.getDate().preZeros(2) +
+			'T' + date.getHours().preZeros(2) + ':' + date.getMinutes().preZeros(2);
+		return Date.parse(str);
+	}
+
 	if(!journeys.length)
 		return null;
-	if(Date.parse(journeys[0].departure) < Date.now()) // 9292 also returns journeys from the past
+	if(Date.parse(journeys[0].departure) < getNow()) // 9292 also returns journeys from the past
 		journeys.shift();
 	if(journeys.length === 1){
 		log.debug('Best journey found',journeys[0]);
@@ -189,29 +198,6 @@ function journeyToString(journey){
 	for(var l = 0; l<legs.length;l++)
 		output += journeyLegToString(legs[l], l === legs.length-1);
 	return output;
-}
-
-
-/**
-* Looks at the hour and day to determine where the user wants to go
-* if not in array, it uses the default location (e.g. home)
-*/
-function getDestination(){
-	log.debug("Entering getDestination");
-	var date = new Date(),
-		day = date.getDay(), //0=sunday
-		hour = date.getHours(),
-		dests = userPreferences.destinations;
-
-	for(var i = 0;i<dests.length;i++)
-		if(dests[i].days) //safety check
-			for(var c = 0; c < dests[i].days.length;c++)
-				if(dests[i].days[c] == day && //if today
-					dests[i].fromTime <= hour &&
-					dests[i].toTime >= hour)
-					return dests[i].location;
-
-	return userPreferences.defaultDestination;
 }
 
 
@@ -329,6 +315,42 @@ function showLoadingScreen(){
 	loading.window.show();
 }
 
+function showSetupScreen(){
+	log.debug('Entering showSetupScreen');
+	// Show splash screen while waiting for data
+	var setup = {};
+	setup.window = new UI.Window();
+	setup.font = 'ROBOTO_BOLD_SUBSET_49';
+	setup.textOverflow = 'wrap';
+	setup.textAlign = 'left';
+	setup.size = new Vector2(144, 168);
+	setup.item = new UI.Text({
+			text:'Open pebble app on phone -> open the settings of this app -> enter destination(s) -> reload app on watch',
+			position: new Vector2(0, 0),
+			color:'white',
+			backgroundColor:'black',
+			size: setup.size,
+			font: setup.font,
+			textOverflow: setup.textOverflow,
+			textAlign: setup.textAlign
+		});
+	setup.window.add(setup.item);
+	setup.window.show();
+}
+//http://developer.getpebble.com/guides/pebble-apps/pebblekit-js/app-configuration/
+Pebble.addEventListener('showConfiguration', function(e) {
+  // Show config page
+  Pebble.openURL(configURL);
+});
+Pebble.addEventListener('webviewclosed',
+  function(e) {
+    var configuration = decodeURIComponent(e.response);
+    log.debug('Configuration window returned: ', configuration);
+	localStorage.destinations = configuration;
+  }
+);
+
+
 /**
 * The next part sets the location to the location variable.
 * The code is based on:
@@ -361,10 +383,6 @@ function updateLocation(){
 }
 
 
-
-//vars to easy update the api, when changed
-var version = '0.1',
-	baseUrl = 'https://api.9292.nl/';
 
 
 
@@ -408,70 +426,93 @@ function getLocTo9292locId(lat, long, callback){
 	);
 }
 
-function getNow(){
-	var date = new Date();
-	return date.getFullYear() + '-' + (date.getMonth() + 1).preZeros(2) + //month start with 0 in js
-		'-' + date.getDate().preZeros(2) +
-		'T' + date.getHours().preZeros(2) + date.getMinutes().preZeros(2);
-	//return date.toJSON().substr(0,7)
-}
-
-
-/**
-* TODO: description..
-*
-* @param {string} from9292loc Current location
-* @param {string} to9292loc Destination string that is a known location to 9292 (e.g. 'station-amsterdam-centraal')
-* For more info:
-* // https://github.com/timvanelsloo/9292-api-spec/blob/master/docs/resources/journeys.md
-*/
-function getJourney(from9292loc,to9292loc,callback){
-	log.debug('Entering getJourney',{from:from9292loc,to:to9292loc});
-	var queryParams = {
-			before : 1,
-			sequence : 1,
-			byFerry : true,
-			bySubway : true,
-			byBus : true,
-			byTram : true,
-			byTrain : true,
-			lang : 'nl-NL', //only option available
-			from : from9292loc,
-			dateTime : getNow(),
-			searchType : 'departure',
-			interchangeTime : userPreferences.extraInterChangeTime?'extra':'standard',
-			after : 5,
-			to : to9292loc
-		},
-		paramStr = $.param(queryParams),
-		URL = baseUrl + version + '/journeys?' + paramStr;
-
-	log.debug('Url in getJourney',{url:URL});
-
-	ajax({
-			url: URL,
-			type: 'json'
-		},
-		function(data) {
-			log.debug('Successfully fetched journey',data);
-			if(callback)
-				callback(data);
-		},
-		function(error) {
-			log.debug('Failed fetching in getJourney', error);
-			if(callback)
-				callback(error);
-		}
-	);
-}
-
 
 function updateScreen(location){
 	log.debug('Entering updateScreen');
 	if(! (location && location.coords))
 		return log.debug('Error, no location found in updateScreen');
 
+	/**
+	* TODO: description..
+	*
+	* @param {string} from9292loc Current location
+	* @param {string} to9292loc Destination string that is a known location to 9292 (e.g. 'station-amsterdam-centraal')
+	* For more info:
+	* // https://github.com/timvanelsloo/9292-api-spec/blob/master/docs/resources/journeys.md
+	*/
+	function getJourney(from9292loc,to9292loc,callback){
+		log.debug('Entering getJourney',{from:from9292loc,to:to9292loc});
+
+		function getNow(){
+			var date = new Date();
+			return date.getFullYear() + '-' + (date.getMonth() + 1).preZeros(2) + //month start with 0 in js
+				'-' + date.getDate().preZeros(2) +
+				'T' + date.getHours().preZeros(2) + date.getMinutes().preZeros(2);
+			//return date.toJSON().substr(0,7)
+		}
+
+		var queryParams = {
+				before : 1,
+				sequence : 1,
+				byFerry : true,
+				bySubway : true,
+				byBus : true,
+				byTram : true,
+				byTrain : true,
+				lang : 'nl-NL', //only option available
+				from : from9292loc,
+				dateTime : getNow(),
+				searchType : 'departure',
+				interchangeTime : userPreferences.extraInterChangeTime?'extra':'standard',
+				after : 5,
+				to : to9292loc
+			},
+			paramStr = $.param(queryParams),
+			URL = baseUrl + version + '/journeys?' + paramStr;
+
+		log.debug('Url in getJourney',{url:URL});
+
+		ajax({
+				url: URL,
+				type: 'json'
+			},
+			function(data) {
+				log.debug('Successfully fetched journey',data);
+				if(callback)
+					callback(data);
+			},
+			function(error) {
+				log.debug('Failed fetching in getJourney', error);
+				if(callback)
+					callback(error);
+			}
+		);
+	}
+
+
 	getLocTo9292locId(location.coords.latitude, location.coords.longitude, function(inp){
+		/**
+		* Looks at the hour and day to determine where the user wants to go
+		* if not in array, it uses the default location (e.g. home)
+		*/
+		function getDestination(){
+			log.debug("Entering getDestination");
+			var date = new Date(),
+				day = date.getDay(), //0=sunday
+				hour = date.getHours(),
+				dests = JSON.parse(localStorage.destinations);
+
+			for(var i = 1;i<dests.length;i++)
+				if(dests[i].days) //safety check
+					for(var c = 0; c < dests[i].days.length;c++)
+						if(dests[i].days[c] == day && //if today
+							dests[i].fromTime <= hour &&
+							dests[i].toTime >= hour)
+							return dests[i].location;
+
+			return dests[0];//defaultDestination
+		}
+
 		var from9292loc = inp,
 			to9292loc = getDestination(),
 			callback = showJourneyDataOnScreen;
@@ -480,8 +521,12 @@ function updateScreen(location){
 }
 
 showLoadingScreen();
-updateLocation(); //triggers the update screen
-setInterval (
-	updateLocation,
-	userPreferences.updateScreenIntervalInSec * 1000
-); //http://stackoverflow.com/questions/26433309/pebble-js-update-view-frequently
+if(!localStorage.destinations)
+	showSetupScreen();
+else{
+	updateLocation(); //triggers the update screen
+	setInterval (
+		updateLocation,
+		userPreferences.updateScreenIntervalInSec * 1000
+	); //http://stackoverflow.com/questions/26433309/pebble-js-update-view-frequently
+}
